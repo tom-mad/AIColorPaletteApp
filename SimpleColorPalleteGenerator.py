d18ffaeae9e5fb7550104139c75d82983311a047
kryptoTomo <tomasz.madej101@gmail.com>
2023-12-14 13:36:03 +0100

    write function which generates colors

--- SimpleColorPalleteGenerator.py
+++ SimpleColorPalleteGenerator.py
@@ -1,14 +1,21 @@
 import openai
 from dotenv import dotenv_values
+import json
+from IPython.display import HTML, display
 
-
-if __name__ == "__main__":
-    config = dotenv_values(".env")
-    client = openai.OpenAI(
+config = dotenv_values(".env")
+client = openai.OpenAI(
     api_key=config["OPENAI_API_KEY"],
-    )
+)
 
-    prompt = """
+def display_colors(colors):
+    display(HTML(" ".join(
+        f"{chr(9608) * 4}"
+        for color in colors
+    )))
+
+def get_and_render_colors(msg):
+    prompt = f"""
 You are a color palette generating assistant that responds to text prompts for color palettes
 Your should generate color palettes that fit the theme, mood, or instructions in the prompt.
 The palettes should be between 2 and 8 colors.
@@ -21,9 +28,8 @@ A: ["#EDF1D6", "#9DC08B", "#609966", "#40513B"]
 
 Desired Format: a JSON array of hexadecimal color codes
 
-Text: a beatuiful sunset
-
-Result:
+Q: Convert the following verbal description of a color palette into a list of colors: {msg}
+A:
 """
     completion = client.chat.completions.create(
         model="gpt-3.5-turbo",
@@ -33,4 +39,6 @@ Result:
         ],
         max_tokens=200
     )
-    print(completion.choices[0].message.content)
+    colors = json.loads(completion.choices[0].message.content)
+    print(colors)
+get_and_render_colors("Thanks giving color palette")
