cb1cd395de9f587f924c04651084c462e5529fec
kryptoTomo <tomasz.madej101@gmail.com>
2023-12-14 13:54:47 +0100

    integrate basic openai to flask app

--- app.py
+++ app.py
@@ -1,4 +1,13 @@
+import json
+import openai
+from dotenv import dotenv_values
 from flask import Flask, render_template, request
+
+config = dotenv_values(".env")
+
+client = openai.OpenAI(
+    api_key=config["OPENAI_API_KEY"],
+)
 
 app = Flask(__name__,
     template_folder='templates'
@@ -6,11 +15,21 @@ app = Flask(__name__,
 
 @app.route("/")
 def index():
-    return "HELLO THERE FROM FLASK!!!"
+    completion = client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", 
+            "content": "Give me a funny word: "}
+        ],
+    )
+    return completion.choices[0].message.content
+    # return render_template("index.html")
 
-@app.route("/dog")
-def dog():
-    return "WOOF WOOF WOOF!!!"
+# @app.route("/palette", methods={"POST"})
+# def prompt_to_palette():
+    # OPEN AI COMPLETION CALL
+
+    # RETURN LIST OF COLORS
 
 if __name__=="__main__":
     app.run(debug=True)
